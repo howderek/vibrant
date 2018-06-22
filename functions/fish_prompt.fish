10c48ada265b2bc104ec7de11b5d78e35e4bc81d
Derek Howard <doombreakr@gmail.com>
2018-06-22 13:48:49 -0500

    Put separator back

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,7 +1,7 @@
 set __fish_git_prompt_color_branch blue
 
 set __fish_git_prompt_char_upstream_prefix ' '
-set __fish_git_prompt_char_stateseparator ''
+set __fish_git_prompt_char_stateseparator ' '
 
 set __fish_git_prompt_show_informative_status true
 set __fish_git_prompt_color_upstream_ahead yellow
